eb94c7a27742a3e604049368c97fc8623c0b9c5c
Volodymyr Holovko <volodymyr.holovko@magnise.com>
2018-06-26 16:40:28 +0300

    to dev

---
+++ src/test/java/Calendar/CalendarLocators.java
@@ -0,0 +1,121 @@
+package Calendar;
+
+public class CalendarLocators {
+    String addresField = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[1]/div";
+    String dateButton = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[2]/div[1]/div";
+    String currentDate = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[2]/div[2]";
+    String addresFieldTitle = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[1]/div/span[1]";
+    String modalCalendarTab = "md-datepicker-calendar-pane";
+    String calendarButton = "marketing-button-group";
+    String calendarHeader = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[3]/div/div/business-calendar-month/div";
+    String staffHeader = "staff-calendar-header";
+    String currentTimeLine = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[3]/div/div/business-calendar-day/div/div[1]";
+    String calendarGrid = "table-day-container";
+    String nextDayButton = "/html/body/app/ui-view/div/ui-view/business-calendar/div/div/div/div[2]/div[1]/div/button[2]";
+    String addBookingButton = "/html/body/app/ui-view/business-header/nav/div[2]/ul/li[1]/a/button";
+    String addBookingPageHeader = "/html/body/app/ui-view/div/ui-view/add-booking-beauty/div/div/div[2]";
+
+    public String getAddBookingPageHeader() {
+        return addBookingPageHeader;
+    }
+
+    public void setAddBookingPageHeader(String addBookingPageHeader) {
+        this.addBookingPageHeader = addBookingPageHeader;
+    }
+
+    public String getAddBookingButton() {
+        return addBookingButton;
+    }
+
+    public void setAddBookingButton(String addBookingButton) {
+        this.addBookingButton = addBookingButton;
+    }
+
+    public String getNextDayButton() {
+        return nextDayButton;
+    }
+
+    public void setNextDayButton(String nextDayButton) {
+        this.nextDayButton = nextDayButton;
+    }
+
+    public String getCalendarGrid() {
+        return calendarGrid;
+    }
+
+    public void setCalendarGrid(String calendarGrid) {
+        this.calendarGrid = calendarGrid;
+    }
+
+    public String getCurrentTimeLine() {
+        return currentTimeLine;
+    }
+
+    public void setCurrentTimeLine(String currentTimeLine) {
+        this.currentTimeLine = currentTimeLine;
+    }
+
+    public String getStaffHeader() {
+        return staffHeader;
+    }
+
+    public void setStaffHeader(String staffHeader) {
+        this.staffHeader = staffHeader;
+    }
+
+    public String getCalendarHeader() {
+        return calendarHeader;
+    }
+
+    public void setCalendarHeader(String calendarHeader) {
+        this.calendarHeader = calendarHeader;
+    }
+
+    public String getCalendarButton() {
+        return calendarButton;
+    }
+
+    public void setCalendarButton(String calendarButton) {
+        this.calendarButton = calendarButton;
+    }
+
+    public String getModalCalendarTab() {
+        return modalCalendarTab;
+    }
+
+    public void setModalCalendarTab(String modalCalendarTab) {
+        this.modalCalendarTab = modalCalendarTab;
+    }
+
+    public String getAddresFieldTitle() {
+        return addresFieldTitle;
+    }
+
+    public void setAddresFieldTitle(String addresFieldTitle) {
+        this.addresFieldTitle = addresFieldTitle;
+    }
+
+    public String getAddresField() {
+        return addresField;
+    }
+
+    public void setAddresField(String addresField) {
+        this.addresField = addresField;
+    }
+
+    public String getDateButton() {
+        return dateButton;
+    }
+
+    public void setDateButton(String dateButton) {
+        this.dateButton = dateButton;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+}
